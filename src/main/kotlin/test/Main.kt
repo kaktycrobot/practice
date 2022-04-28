ea72d737db0cf8a31f422ede54eb92793bba0e6f
Sergey Ivanov <cthulhudark@gmail.com>
2022-04-28 22:00:14 +0300

    add ide to fedora

--- src/main/kotlin/test/Main.kt
+++ src/main/kotlin/test/Main.kt
@@ -1,12 +1,4 @@
 package test
-
-import java.util.SortedMap
-
 fun main() {
-    val testList = MutableList(10) { (-20..20).random() }
-    println(testList)
-    val filtered = testList.filterNot { it > 0 }
-
-    println(filtered)
-
+    ku ku epta
 }
