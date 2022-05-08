4cc5a8aa34bf4dc33bf7b64075a15533433b111c
kaktycrobot <cthulhudark@gmail.com>
2022-05-08 16:53:12 +0300

    module10 done

--- src/main/kotlin/module10/lesson3/Main.kt
+++ src/main/kotlin/module10/lesson3/Main.kt
@@ -3,7 +3,35 @@ package module10.lesson3
 import module10.lesson1.Callable
 
 fun main() {
-    val anonymous = object : Callable {
+//    val anonymous = object : Callable {
+//        val name = "anonymous"
+//        val lastName = "object"
+//        fun method() = println("anonymous object")
+//        override val vendor: String = "anonymous vendor"
+//
+//        override fun call(number: String) {
+//            println("anonymous object calling")
+//        }
+//    }
+//
+//    anonymous.name
+//    anonymous.lastName
+//    anonymous.method()
+//
+//    val test = AnonymousTest()
+//    test.testMethod(anonymous)
+    val evenChecker = IntPredicate { i -> i % 2 == 0 }
+
+    println("8 is even - ${evenChecker.accept(8)}")
+    println("9 is even - ${evenChecker.accept(9)}")
+}
+
+fun interface IntPredicate {
+    fun accept(i: Int): Boolean
+}
+
+class AnonymousTest {
+    private val anonymous = object : Callable {
         val name = "anonymous"
         val lastName = "object"
         fun method() = println("anonymous object")
@@ -14,7 +42,16 @@ fun main() {
         }
     }
 
-    anonymous.name
-    anonymous.lastName
-    anonymous.method()
+    fun testMethod(obj: Callable) {
+        obj.vendor
+//        obj.name
+
+
+//        anonymous.name
+//        anonymous.lastName
+//        anonymous.method()
+//        anonymous.vendor
+//        anonymous.call("+7(955)666-33-22")
+
+    }
 }
---
+++ src/main/kotlin/module10/lesson4/Main.kt
@@ -0,0 +1,9 @@
+package module10.lesson4
+
+fun main() {
+    sum(4, 8)
+    sum(3.2, 5.0)
+}
+
+fun sum(a: Int, b: Int) = a + b
+fun sum(a: Double, b: Double) = a + b
