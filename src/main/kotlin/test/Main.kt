8edbcf1b4fa2eef564beca46d6c6df668cbc6b4c
kaktycrobot <cthulhudark@gmail.com>
2022-05-18 22:12:14 +0300

    test added

--- src/main/kotlin/test/Main.kt
+++ src/main/kotlin/test/Main.kt
@@ -1,3 +1,12 @@
 package test
+
+enum class RGB { RED, GREEN, BLUE }
+
+inline fun <reified T : Enum<T>> printAllValues() {
+    print(enumValues<T>().joinToString { it.name })
+}
+
+
 fun main() {
+    printAllValues<RGB>()
 }
